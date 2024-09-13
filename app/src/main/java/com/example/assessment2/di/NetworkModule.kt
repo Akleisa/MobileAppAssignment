f4a296b74adf5dc3bfbcec643799821a542f79eb
elizatlai <leliza777@gmail.com>
2024-09-13 16:08:51 +1000

    set up application class and hilt module

---
+++ app/src/main/java/com/example/assessment2/di/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.example.assessment2.di
+
+import com.example.assessment2.network.ApiService
+import com.example.assessment2.network.RetrofitInstance
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        return RetrofitInstance.apiService
+    }
+}
