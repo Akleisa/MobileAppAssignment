a64e896737fcfc69a917ade9d23b21a63ba0f05e
elizatlai <leliza777@gmail.com>
2024-09-13 15:56:02 +1000

    inject constructors

--- app/src/main/java/com/example/assessment2/repository/DashboardRepository.kt
+++ app/src/main/java/com/example/assessment2/repository/DashboardRepository.kt
@@ -1,9 +1,11 @@
 package com.example.assessment2.repository
 
 import com.example.assessment2.network.ApiService
+import javax.inject.Inject
 
-class DashboardRepository(private val apiService: ApiService) {
-
+class DashboardRepository @Inject constructor(
+    private val apiService: ApiService
+) {
     // Function to get dashboard data based on keypass
     suspend fun getDashboardData(keypass: String) = apiService.getDashboardData(keypass)
 }
